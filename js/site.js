8e4389e14efcf6f7678bc5c7ac6ac6e0b63644e4
microsnow <43006026+byteflying@users.noreply.github.com>
2021-11-02 09:18:42 +0800

    Add files via upload

---
+++ js/site.js
@@ -0,0 +1,45 @@
+﻿function reconvert(str) {
+    str = str.replace(/(\\u)(\w{1,4})/gi, function ($0) {
+        return (String.fromCharCode(parseInt((escape($0).replace(/(%5Cu)(\w{1,4})/g, "$2")), 16)));
+    });
+    str = str.replace(/(&#x)(\w{1,4});/gi, function ($0) {
+        return String.fromCharCode(parseInt(escape($0).replace(/(%26%23x)(\w{1,4})(%3B)/g, "$2"), 16));
+    });
+    str = str.replace(/(&#)(\d{1,6});/gi, function ($0) {
+        return String.fromCharCode(parseInt(escape($0).replace(/(%26%23)(\d{1,6})(%3B)/g, "$2")));
+    });
+
+    return str;
+}
+
+function showMessage(msg, type = "error") {
+    toastr.options = {
+        "closeButton": false,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": false,
+        "positionClass": "toast-top-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    };
+
+    if (type === "error") {
+        toastr.error(msg);
+    } else if (type === "success") {
+        toastr.success(msg);
+    }
+    else if (type === "warning") {
+        toastr.warning(msg);
+    }
+    else if (type === "info") {
+        toastr.info(msg);
+    }
+}
